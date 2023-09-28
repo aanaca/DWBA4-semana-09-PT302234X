bc00e0e9c72bc0d0b1c244816bcadb719237c1e1
aanaca <140763719+aanaca@users.noreply.github.com>
2023-09-28 23:22:01 +0000

    DWBA4

--- js/disabling.js
+++ js/disabling.js
@@ -1,43 +1,62 @@
-	/**
-	    * Disable right-click of mouse, F12 key, and save key combinations on page
-	    * By Arthur Gareginyan (arthurgareginyan@gmail.com)
-	    * For full source code, visit https://mycyberuniverse.com
-	    * https://mycyberuniverse.com/developing/disabling-right-clicking-by-using-javascript.html
-	    */
-	  window.onload = function() {
-	    document.addEventListener("contextmenu", function(e){
-	      e.preventDefault();
-	    }, false);
-	    document.addEventListener("keydown", function(e) {
-	    //document.onkeydown = function(e) {
-	      // "I" key
-	      if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-	        disabledEvent(e);
-	      }
-	      // "J" key
-	      if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-	        disabledEvent(e);
-	      }
-	      // "S" key + macOS
-	      if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-	        disabledEvent(e);
-	      }
-	      // "U" key
-	      if (e.ctrlKey && e.keyCode == 85) {
-	        disabledEvent(e);
-	      }
-	      // "F12" key
-	      if (event.keyCode == 123) {
-	        disabledEvent(e);	
-     	 }
-    	}, false);
-    	function disabledEvent(e){
-	      if (e.stopPropagation){
-        	e.stopPropagation();
-      	} else if (window.event){
-        	window.event.cancelBubble = true;
-      	}
-      	e.preventDefault();
-      	return false;
-    	}
-  	};
+/**
+    * Disable right-click of mouse, F12 key, and save key combinations on page
+    * By Arthur Gareginyan (arthurgareginyan@gmail.com)
+    * For full source code, visit https://mycyberuniverse.com
+    * https://mycyberuniverse.com/developing/disabling-right-clicking-by-using-javascript.html
+    */
+window.onload = function() {
+  document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+  }, false);
+  document.addEventListener("keydown", function(e) {
+    //document.onkeydown = function(e) {
+    // "I" key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+      disabledEvent(e);
+    }
+    // "J" key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+      disabledEvent(e);
+    }
+    // "S" key + macOS
+    if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      disabledEvent(e);
+    }
+    // "U" key
+    if (e.ctrlKey && e.keyCode == 85) {
+      disabledEvent(e);
+    }
+    // "F12" key
+    if (event.keyCode == 123) {
+      disabledEvent(e);
+    }
+  }, false);
+  function disabledEvent(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    } else if (window.event) {
+      window.event.cancelBubble = true;
+    }
+    e.preventDefault();
+    return false;
+  }
+};
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
